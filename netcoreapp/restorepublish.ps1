c9ddc2a78dd74a267368c9dc12d479a85893fdcc
Wes Haggard <Wes.Haggard@microsoft.com>
2016-11-03 09:26:14 -0700

    Microsoft.NETCore.App 1.1.0 with netcoreapp1.1 tfm

--- netcoreapp/restorepublish.ps1
+++ netcoreapp/restorepublish.ps1
@@ -1,7 +1,7 @@
 dotnet restore
-dotnet publish -f netcoreapp1.0 -r win10-x64
-dotnet publish -f netcoreapp1.0 -r osx.10.10-x64
-dotnet publish -f netcoreapp1.0 -r ubuntu.14.04-x64
+dotnet publish -f netcoreapp1.1 -r win10-x64
+dotnet publish -f netcoreapp1.1 -r osx.10.10-x64
+dotnet publish -f netcoreapp1.1 -r ubuntu.14.04-x64
 
 $outputDir = $args[0]
 
@@ -16,4 +16,4 @@ if ((Test-Path $outputDir))
 
 mkdir $outputDir
 copy .\project.lock.json $outputDir
-move .\bin\Debug\netcoreapp1.0\* $outputDir
+move .\bin\Debug\netcoreapp1.1\* $outputDir
