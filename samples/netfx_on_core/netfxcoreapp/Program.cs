32109d6109bde376d1d1ac5f43785edc7080b0e9
Wes Haggard <Wes.Haggard@microsoft.com>
2016-11-03 18:43:07 -0700

    Add sample netfx console application running on .NET Core

---
+++ samples/netfx_on_core/netfxcoreapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFXConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Testing 123");
+            Console.WriteLine($"Running on core assembly {typeof(object).Assembly.Location}.");
+        }
+    }
+}
