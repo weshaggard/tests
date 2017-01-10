742a1170de62079d1896429dfec4dd63de2d6253
Wes Haggard <Wes.Haggard@microsoft.com>
2017-01-10 10:44:10 -0800

    Add ServerPublish to tests repo
    Port from https://github.com/weshaggard/corefxlab/commit/a99938c604195a9bd1c42ea72f6305dbad0a07eb

---
+++ publish/ServerPublish/get-dotnetcore.ps1
@@ -0,0 +1,37 @@
+﻿[CmdletBinding()]
+param (
+
+    [ValidateNotNullOrEmpty()]
+    [String] $Path = ".\_DotNetCore10",
+
+    [ValidateNotNullOrEmpty()]
+    [string] $NugetExe = "c:\temp\nuget\3.5\nuget.exe",
+
+    [ValidateNotNullOrEmpty()]
+    [string] $SymchkExe = "d:\pk\src\tools\x86\symchk.exe"
+)
+
+$ErrorActionPreference = 'Stop'
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$DownloadDir = $Path
+if (Test-Path $DownloadDir)
+{
+    rmdir $DownloadDir -Recurse
+}
+mkdir $DownloadDir
+
+
+$ProjDir = Get-ScriptDirectory
+Copy-Item (Join-Path $ProjDir "project.json") $DownloadDir
+Copy-Item (Join-Path $ProjDir "download.proj") $DownloadDir
+
+$Archs = @("x86", "x64", "arm")
+$Archs | % {
+    msbuild ".\download.proj" /p:TargetArch=$_ /p:NuGetToolPath=$NugetExe /p:SymChkToolPath=$SymchkExe /flp:v=diag`;LogFile=msbuild.$_.log
+}
