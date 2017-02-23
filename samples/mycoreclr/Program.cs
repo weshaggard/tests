316b6804eac753470bf98549f529b5a15f2404b1
Wes Haggard <Wes.Haggard@microsoft.com>
2017-02-23 10:21:45 -0800

    Sample consuming custom runtime package.
    Using CLI version 2.0.0-alpha-004853.

---
+++ samples/mycoreclr/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coreAssemblyInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(typeof(object).Assembly.Location);
+            Console.WriteLine($"Hello World from Core {coreAssemblyInfo.ProductVersion}");
+        }
+    }
+}
