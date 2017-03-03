6b0803d5c95581f35e730a2a0944f93a14aa3998
Wes Haggard <Wes.Haggard@microsoft.com>
2017-03-02 16:06:22 -0800

    Add NS20 sample project

---
+++ samples/ns20/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ns20
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            System.Xml.XmlReader r = System.Xml.XmlReader.Create((System.IO.Stream)null);
+        }
+    }
+}
