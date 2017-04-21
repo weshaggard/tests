8b0f46b9bd44d6b2a4506c5648d774a0cbf40a61
Wes Haggard <Wes.Haggard@microsoft.com>
2017-04-21 14:01:22 -0700

    Use local cli instead of globally installed version

--- netcoreapp/restorepublish.ps1
+++ netcoreapp/restorepublish.ps1
@@ -1,12 +1,13 @@
 param (
 $targetFramework = "netcoreapp1.1",
-$ncaPackageVersion = "1.1.1"
+$ncaPackageVersion = "1.1.1",
+$dotnetPath = "d:\cli\dotnet.exe"
 )
 
-dotnet restore -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-dotnet publish -f $targetFramework -r win10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-dotnet publish -f $targetFramework -r osx.10.10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-dotnet publish -f $targetFramework -r ubuntu.14.04-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath restore -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r win10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r osx.10.10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r ubuntu.14.04-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
 
 $outputDir = $targetFramework + "-" + $ncaPackageVersion
 
