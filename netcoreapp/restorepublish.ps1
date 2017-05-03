95809600035ae6f69cdf3b6d612f55cf50d3ecfb
Wes Haggard <Wes.Haggard@microsoft.com>
2017-05-03 15:45:15 -0700

    Restore .NET Core 2.0 preview1 build (prior to releasing)
    Changed to portable RIDs

--- netcoreapp/restorepublish.ps1
+++ netcoreapp/restorepublish.ps1
@@ -5,9 +5,9 @@ $dotnetPath = "d:\cli\dotnet.exe"
 )
 
 & $dotnetPath restore -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r win10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r osx.10.10-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r ubuntu.14.04-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r win-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r osx-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r linux-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
 
 $outputDir = $targetFramework + "-" + $ncaPackageVersion
 
