6aaf22e3bf0e92c632d8f070d51843db3542a443
Wes Haggard <Wes.Haggard@microsoft.com>
2017-05-09 10:53:39 -0700

    Update asset files for the 1.0.5 and 1.1.2 releases.

--- netcoreapp/restorepublish.ps1
+++ netcoreapp/restorepublish.ps1
@@ -4,10 +4,21 @@ $ncaPackageVersion = "1.1.2",
 $dotnetPath = "d:\cli\dotnet.exe"
 )
 
+$winRID = "win-x64";
+$osxRID = "osx-x64";
+$linuxRID = "linux-x64";
+
+if ($targetFramework -eq "netcoreapp1.0" -or $targetFramework -eq "netcoreapp1.1")
+{
+    $winRID = "win10-x64";
+    $osxRID = "osx.10.10-x64";
+    $linuxRID = "ubuntu.14.04-x64";
+}
+
 & $dotnetPath restore -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r win-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r osx-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
-& $dotnetPath publish -f $targetFramework -r linux-x64 -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r $winRID -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r $osxRID -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
+& $dotnetPath publish -f $targetFramework -r $linuxRID -p:TargetFramework=$targetFramework -p:RuntimeFrameworkVersion=$ncaPackageVersion
 
 $outputDir = $targetFramework + "-" + $ncaPackageVersion
 
